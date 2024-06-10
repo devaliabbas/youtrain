528b86547478edc1d1840884d1fb3d8d1467d632
Ali F. Abbas <alifuadabbas2@gmail.com>
2024-06-10 17:17:02 +0300

    Dashboard form complete

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,33 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler, useFieldArray } from "react-hook-form"
+import { FaTrashCan, FaPlus } from "react-icons/fa6"
 
 type Inputs = {
   question: string
-  options: string[]
+  options: { value: string }[]
+  answer: number
+  flag: string
+  year: number
+  session: number
+  specialization: string
+  image: File
 }
 
 const Dashboard = () => {
-  const { register, handleSubmit } = useForm<Inputs>()
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({
+    defaultValues: {
+      options: [{ value: "" }, { value: "" }, { value: "" }, { value: "" }], // Default values for the options
+    },
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "options",
+  })
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data)
@@ -16,70 +37,82 @@ const Dashboard = () => {
     <div className="mt-16">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="px-4 mt-20">
-          <p className="text-xl mb-2">السؤال:</p>
+          <p className="text-xl mb-2">
+            السؤال: <span className="text-red-500">*</span>
+          </p>
           <input
-            {...register("question")}
+            {...register("question", { required: true })}
             type="text"
             className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
           />
+          {errors.question && (
+            <span className="text-red-500">هذا الحقل مطلوب.</span>
+          )}
         </div>
         <div className="px-4 mt-4">
-          <p className="text-xl mb-2">الخيارات:</p>
-          <div className="flex justify-center items-center">
-            <p className="ml-2 text-xl">0</p>
-            <input
-              {...register("options.0")}
-              type="text"
-              className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
+          <div className="flex justify-between items-center mb-2">
+            <p className="text-xl mb-2">
+              الخيارات: <span className="text-red-500">*</span>
+            </p>
+            <FaPlus
+              size={25}
+              onClick={() => append({ value: "" })}
+              className="bg-green-500 text-white font-bold rounded-lg w-12 h-8 p-1"
             />
           </div>
-          <div className="flex justify-center items-center">
-            <p className="ml-2 text-xl">1</p>
-            <input
-              {...register("options.1")}
-              type="text"
-              className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <p className="ml-2 text-xl">2</p>
-            <input
-              {...register("options.2")}
-              type="text"
-              className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <p className="ml-2 text-xl">3</p>
-            <input
-              {...register("options.3")}
-              type="text"
-              className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
-            />
-          </div>
-          <div className="flex justify-center items-center">
-            <p className="ml-2 text-xl">4</p>
-            <input
-              {...register("options.4")}
-              type="text"
-              className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
-            />
-          </div>
+          {fields.map((field, index) => (
+            <div
+              key={field.id}
+              className="flex flex-col justify-center items-start"
+            >
+              <div className="flex justify-center items-center w-full">
+                <p className="ml-2 text-xl">{index}</p>
+                <input
+                  {...register(`options.${index}.value` as const, {
+                    required: true,
+                  })}
+                  type="text"
+                  className="border-slate-400 my-2 border-2 rounded-xl p-2 w-full focus:outline-none"
+                />
+                <FaTrashCan
+                  size={25}
+                  onClick={() => remove(index)}
+                  className="text-red-500 font-bold mr-2 -ml-1"
+                />
+              </div>
+              {errors.options && errors.options[index] && (
+                <span className="text-red-500 mr-6">هذا الحقل مطلوب.</span>
+              )}
+            </div>
+          ))}
         </div>
         <div className="px-4 mt-4">
-          <p className="text-xl mb-2">الجواب:</p>
-          <select className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none">
+          <p className="text-xl mb-2">
+            الجواب: <span className="text-red-500">*</span>
+          </p>
+          <select
+            {...register("answer", { required: true })}
+            className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
+          >
             <option value=""></option>
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
+            {fields.map((_item, index) => (
+              <option key={index} value={index}>
+                {index}
+              </option>
+            ))}
           </select>
+          {errors.answer && (
+            <span className="text-red-500">هذا الحقل مطلوب.</span>
+          )}
         </div>
         <div className="px-4 mt-4">
-          <p className="text-xl mb-2">المحور:</p>
-          <select className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none">
+          <p className="text-xl mb-2">
+            المحور: <span className="text-red-500">*</span>
+          </p>
+          <select
+            {...register("flag", { required: true })}
+            className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
+          >
             <option value=""></option>
             <option value="برمجيات">برمجيات</option>
             <option value="شبكات">شبكات</option>
@@ -87,17 +120,24 @@ const Dashboard = () => {
             <option value="قواعد بيانات">قواعد بيانات</option>
             <option value="ذكاء صنعي">ذكاء صنعي</option>
           </select>
+          {errors.question && (
+            <span className="text-red-500">هذا الحقل مطلوب.</span>
+          )}
         </div>
         <div className="px-4 mt-4">
-          <p className="text-xl mb-2">السنة:</p>
+          <p className="text-xl mb-2">السنة: </p>
           <input
+            {...register("year")}
             type="text"
             className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
           />
         </div>
         <div className="px-4 mt-4">
           <p className="text-xl mb-2">الدورة:</p>
-          <select className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none">
+          <select
+            {...register("session")}
+            className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
+          >
             <option value=""></option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -105,7 +145,10 @@ const Dashboard = () => {
         </div>
         <div className="px-4 mt-4">
           <p className="text-xl mb-2">الإختصاص:</p>
-          <select className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none">
+          <select
+            {...register("specialization")}
+            className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
+          >
             <option value=""></option>
             <option value="تخرج">تخرج</option>
             <option value="برمجيات">برمجيات</option>
@@ -116,6 +159,7 @@ const Dashboard = () => {
         <div className="px-4 mt-4">
           <p className="text-xl mb-2">الصورة:</p>
           <input
+            {...register("image")}
             type="file"
             className="border-slate-400 border-2 rounded-xl p-2 w-full focus:outline-none"
           />
